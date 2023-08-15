3f58ba6009a8cfa6e8306f78626e844bbfa566cb
AbedSoleymani <zsoleymani@ualberta.ca>
2023-08-15 10:23:21 -0600

    finalizing linked-list.py

--- linked-list.py
+++ linked-list.py
@@ -1,17 +1,79 @@
-# %%
-
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, data):
+        self.data = data
         self.next = None
-   
-head = Node(2) 
-head.next = Node(1)
-head.next.next = Node(4)
 
-print(head.value)
-print(head.next.next.value)
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-# %%
-from collections import deque
-my_deque = deque(maxlen=100)
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=' -> ')
+            current = current.next
+        print("None")
+
+    def search(self, target):
+        current = self.head
+        while current:
+            if current.data == target:
+                return True
+            current = current.next
+        return False
+
+    def prepend(self, data):
+        """Insertion at the beginning"""
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, target):
+        if not self.head:
+            return
+        if self.head.data == target:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == target:
+                current.next = current.next.next
+                return
+            current = current.next
+
+
+if __name__ == "__main__":
+    import os
+    os.system("clear")
+
+    print("basic approach:")
+    head = Node(2) 
+    head.next = Node(1)
+    head.next.next = Node(4)
+    print(head.next.next.data)
+
+    print("\nadvanced approach:")
+    my_linked_list = LinkedList()
+    my_linked_list.append(data=1)
+    my_linked_list.append(data=7)
+    my_linked_list.append(data=3)
+    my_linked_list.display()
+
+    my_linked_list.prepend(data=-8)
+    my_linked_list.display()
+
+    search_result = my_linked_list.search(target=7)
+    print(search_result)
+
+    my_linked_list.delete(target=1)
+    my_linked_list.display()
